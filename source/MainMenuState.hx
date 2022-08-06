99c475adee25354c5cf627ea643423a22ef11365
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-08-05 21:43:27 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -234,7 +234,7 @@ class MainMenuState extends MusicBeatState
 										PlayState.storyPlaylist = ['masturbasuceso'];
 										PlayState.isStoryMode = false;
 
-										PlayState.SONG = Song.loadFromJson(PlayState.storyPlaylist[0].toLowerCase() + '-normal', PlayState.storyPlaylist[0].toLowerCase());
+										PlayState.SONG = Song.loadFromJson(PlayState.storyPlaylist[0].toLowerCase() + '-pajero', PlayState.storyPlaylist[0].toLowerCase());
 										PlayState.campaignScore = 0;
 										PlayState.campaignMisses = 0;
 										LoadingState.loadAndSwitchState(new PlayState(), true);
